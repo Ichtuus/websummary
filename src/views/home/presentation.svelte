cd07f82f868e496c366dc46018152dc936cc51c0
Nicolas Diarra <nicolasdiarra777@gmail.com>
2024-09-19 21:34:58 +0200

    feat: test for new img

--- src/views/home/presentation.svelte
+++ src/views/home/presentation.svelte
@@ -56,7 +56,7 @@
 		>
 			<img
 				class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-				src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+				src="/websummary_ext.png"
 				alt=""
 			/>
 		</div>
