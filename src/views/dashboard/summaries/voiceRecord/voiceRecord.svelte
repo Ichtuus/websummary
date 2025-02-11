5afee5704d85acd0d03141865a22120492767be3
Nicolas Diarra <nicolasdiarra777@gmail.com>
2025-02-11 23:54:17 +0100

    feat: wip

--- src/views/dashboard/summaries/voiceRecord/voiceRecord.svelte
+++ src/views/dashboard/summaries/voiceRecord/voiceRecord.svelte
@@ -4,16 +4,10 @@
 	import { MediaRecorderService } from './services/mediaRecorder';
 	import VoiceRecordLoader from './components/voiceRecordLoader.svelte';
 
+	// TODO : make http route for manage the summarise of the record and give the possibility to save the recording (inside DB) or remove
+
 	const recorder = new MediaRecorderService();
 	const recorderStore = recorder.recorderStore;
-
-	const startRecord = () => {
-		recorder.startRecording();
-	};
-
-	const stopRecord = () => {
-		recorder.stopRecording();
-	};
 
 	onDestroy(() => {
 		recorder.clearRecording();
@@ -28,12 +22,12 @@
 
 		<button
 			class="w-8 h-8 dark:bg-gray-800 rounded-full justify-items-center text-white"
-			on:click={stopRecord}
+			on:click={() => recorder.stopRecording()}
 			id="stop"><svelte:component this={StopSolid} /></button
 		>
 	{:else if !$recorderStore.audioURL}
 		<button
-			on:click={startRecord}
+			on:click={() => recorder.startRecording()}
 			id="play"
 			class="w-8 h-8 dark:bg-gray-800 rounded-full justify-items-center text-white"
 			><svelte:component this={PlaySolid} /></button
